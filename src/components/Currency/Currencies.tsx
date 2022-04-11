73c85854acd3467ba27fb76aaec6ed20534e1fb7
Alexey Biruykov <alexbirwp@gmail.com>
2022-04-11 21:53:08 +0300

    Added sort

--- src/components/Currency/Currencies.tsx
+++ src/components/Currency/Currencies.tsx
@@ -8,7 +8,9 @@ import { Valute } from "./CurrencyListElement";
 const Currencies: React.FC = () => {
 
     const [valutes, setValutes] = useState<Valute[]>([]);
+
     const [filter, setFilter] = useState('');
+    const [sort, setSort] = useState('');
 
     useEffect(() => {
         getCurrencyList(setValutes);
@@ -17,16 +19,34 @@ const Currencies: React.FC = () => {
     const filterChangeHandler = (newFilter : string) => {
         setFilter(newFilter)
     }
+    const sortChangeHandler = (newSort: string) => {
+        setSort(newSort);
+    }
 
-    const filteredValutes = valutes.filter(valute => {
+    let sortedValutes = valutes;
+
+    if(sortedValutes.length && sort !== '') {
+        const numberSort = Number(sort);
+        sortedValutes = valutes.sort((valute1, valute2) => {
+            const delta1 = valute1.Value - valute1.Previous;
+            const delta2 = valute2.Value - valute2.Previous;
+            return (delta1 - delta2) * numberSort;
+        });
+    }
+    
+
+
+    const filteredValutes = sortedValutes.filter(valute => {
         const isNameContains = valute.Name.toLowerCase().includes(filter.toLowerCase());
         const isCharCodeContains = valute.CharCode.toLowerCase().includes(filter.toLowerCase())
         return isNameContains || isCharCodeContains;
-    })
+    });
 
     return (
         <Container>
-            <CurrencyFilter onFilterChange={filterChangeHandler}/>
+            <CurrencyFilter 
+            onSortChange={sortChangeHandler}
+            onFilterChange={filterChangeHandler}/>
             <CurrencyList valutes={filteredValutes}/>
         </Container>
     );
--- src/components/Currency/CurrencyFilter.tsx
+++ src/components/Currency/CurrencyFilter.tsx
@@ -1,15 +1,20 @@
 import styles from './CurrencyFilter.module.css'
 
 interface  CurrencyFilterProps {
-    onFilterChange: (filter: string) => void
+    onFilterChange: (filter: string) => void;
+    onSortChange: (sort: string) => void;
 }
 
 
-const CurrencyFilter: React.FC<CurrencyFilterProps> = ({onFilterChange}) => {
+const CurrencyFilter: React.FC<CurrencyFilterProps> = ({onFilterChange, onSortChange}) => {
     const filterChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-        event.preventDefault();
         onFilterChange(event.target.value);
     }
+
+    const sortChangeHandler: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
+        onSortChange(event.target.value)
+    }
+    
     return (
         <div className={styles['filter-wrapper']}>
             <input 
@@ -17,6 +22,11 @@ const CurrencyFilter: React.FC<CurrencyFilterProps> = ({onFilterChange}) => {
             onChange={filterChangeHandler}
             type="text" 
             />
+            <select onChange={sortChangeHandler}>
+                <option value="">Сортировка</option>
+                <option value="-1">Рост(Убывание)</option>
+                <option value="1">Сортировка(Возрастание)</option>
+            </select>
         </div>
     );
 }
